c0cbfab41366a29c7633b53791cb8ac14bf5c7fd
Oleh <olehusofa@hotmail.com>
2024-11-26 17:18:13 -0500

    Updated

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-    theme: {
-      extend: {
-        fontFamily: {
-          sans: ['Inter', 'sans-serif'],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Oxygen-Sans', 'Ubuntu', 'Cantarell', 'Helvetica Neue', 'sans-serif'],
+      },
+      fontSize: {
+        'xs': ['0.75rem', { lineHeight: '1rem' }],
+        'sm': ['0.8125rem', { lineHeight: '1.25rem' }],
+        'base': ['0.875rem', { lineHeight: '1.5rem' }],
+        'lg': ['1rem', { lineHeight: '1.75rem' }],
+        'xl': ['1.125rem', { lineHeight: '1.75rem' }],
+        '2xl': ['1.25rem', { lineHeight: '2rem' }],
+        '3xl': ['1.5rem', { lineHeight: '2rem' }],
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            fontSize: '0.875rem',
+            h1: {
+              fontSize: '1.25rem',
+            },
+            h2: {
+              fontSize: '1.125rem',
+            },
+            h3: {
+              fontSize: '1rem',
+            },
+          },
         },
       },
     },
-    plugins: [
-      require('@tailwindcss/typography'),
-    ],
-  }
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+}
